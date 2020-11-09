5d6f45f0204e5c558efc7c573774648cbf3149ba
Gustavo Pereira <gustavopereirasantos@hotmail.com>
2020-11-08 23:08:23 -0300

    App Created!

---
+++ AppQuantidade/AppQuantidade/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppQuantidade
+{
+	public partial class MainPage : ContentPage
+	{
+		int quantidade = 0;
+
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		private void Aumentar(object sender, EventArgs e)
+		{
+			quantidade++;
+			lblQuantidade.Text = quantidade.ToString();
+		}
+
+		private void Diminuir(object sender, EventArgs e)
+		{
+			if (quantidade == 0)
+				return;
+
+			quantidade--;
+			lblQuantidade.Text = quantidade.ToString();
+		}
+	}
+}
